9378383c15e59639f189d29a5da709d20cd1deb8
Augusto Prestes <fenkzebra147@gmail.com>
2022-11-02 15:06:27 -0300

    Testando as demais funcoes para a tela do projeto

--- functions.py
+++ functions.py
@@ -1,11 +1,4 @@
-
 import random as rd
 
 # Funcao para gerar as senha aleatorias
-def Gerar_senha(quantidade):
-    chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*<>|?'
-    chars_list= rd.choices(chars, k=quantidade) 
-    senha = ''.join(chars)
-    return senha
 
- # 
--- inteface.py
+++ inteface.py
@@ -1,5 +1,7 @@
 # importando biblioteca 
 import PySimpleGUI as sg  # modulo da interface grafica
+import random 
+# from functions import Gerar_senha # funcao para gerrar a senha aleatoria
 
 """
 Sera necessario importar as funcoes criadas para gerar as senhas e os nomes, pois e necessario criar um objeto com a interface
@@ -11,12 +13,13 @@ class PyProject:
 
         # Passando as informacoes do layout da interface
         layout = [
-            [sg.Text('Informe seu nome:', size=(30, 1)), sg.InputText(key='nome', size=(30, 1))],
-            [sg.Text('Qual o tamanho da senha:', size=(20, 1)), sg.Combo(values=[8, 11, 15], key='qtd_caracter', default_value=8,size=(2, 1))],
+            [sg.Text('Informe seu nome:', size=(20, 1)), sg.InputText(key='nome', size=(35, 1))],
+            [sg.Text('Informe o site do login:', size=(20, 1)), sg.InputText(key='site', size=(35, 1))],
+            [sg.Text('Qual o tamanho da senha:', size=(20, 1)), sg.Combo(values=[8, 11, 15], key='qtd_caracter', default_value=8, size=(2, 1))],
             [sg.Text('Console do Gerador:')],
             [sg.Output(size=(60, 2))],
-            [sg.Text('Logins Salvos:')],
-            [sg.Output(size=(60, 10))],
+            #[sg.Text('Logins Salvos:')],
+            #[sg.Output(size=(60, 10))],
             [sg.Button('Gerrar Login')]
         ]
 
@@ -28,8 +31,28 @@ class PyProject:
     def Iniciar(self):
         while True:
             evento, valores = self.janela.read()
-            if evento == sg.Window_closed:
+            if evento == sg.WINDOW_CLOSED:
                 break
+            if evento == 'Gerrar Senha':
+                nova_senha = self.Gerar_senha(valores)
+                self.Salvar_Senha(nova_senha, valores)
+
+    def Gerar_senha(self, valores):
+        char_list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*'
+        chars = random.choices(char_list, k=int(valores['qtd_caracter']))
+        new_pass = ''.join(chars)
+        return new_pass
+
+    def Salvar_Senha(self, nova_senha, valores):
+        if valores['site'] == '' or valores['nome'] == '':
+            print("Preencha todos os campos para gerar sua nova senha!")
+
+        else:
+            with open('Senhas.txt', 'a', newline='') as arquivo:
+                arquivo.write(f"\nSenha Salva para o Site: {valores['site']}, com o usuario: {valores['nome']}, com a senha: {nova_senha}")
+            print(nova_senha)
+            print("Arquivo salvo com Sucesso em Senhas.txt")
+
 
 project = PyProject()
 
